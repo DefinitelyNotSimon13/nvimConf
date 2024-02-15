8b2d2cfb3287a1dc495b3798ec2ad765ab47bca7
DefinitelyNotSimon13 <simon21.blum@gmail.com>
2024-02-15 09:58:15 +0100

    Fix lsp config

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -41,18 +41,6 @@ M.capabilities.textDocument.completion.completionItem = {
 require("lspconfig").lua_ls.setup {
   on_attach = M.on_attach,
   capabilities = M.capabilities,
-  cmd = {
-    -- see clangd --help-hidden
-    "clangd",
-    "--background-index",
-    -- by default, clang-tidy use -checks=clang-diagnostic-*,clang-analyzer-*
-    -- to add more checks, create .clang-tidy file in the root directory
-    -- and add Checks key, see https://clang.llvm.org/extra/clang-tidy/
-    "--clang-tidy",
-    "--completion-style=bundled",
-    "--cross-file-rename",
-    "--header-insertion=iwyu",
-  },
   settings = {
     Lua = {
       diagnostics = {
